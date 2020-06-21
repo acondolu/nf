8f20265790437713c0bd91f5f80b24cb3460aabd
Andrea Condoluci <andrea.condoluci@tweag.io>
2020-06-21 23:43:39 +0200

    refactor and setoids/morphisms

---
+++ src/NFO/Main.v
@@ -0,0 +1,26 @@
+Module NFO.
+
+Add LoadPath "src/NFO/".
+Require Eeq.
+Require Model.
+Require Iin.
+Require Ext2.
+
+(*  *)
+Definition set := Model.set.
+
+(* Equality *)
+Definition eeq := Eeq.eeq.
+Definition eeq_refl : forall x, eeq x x := @Eeq.eeq_refl.
+Definition eeq_sym : forall x y, eeq x y -> eeq y x := @Eeq.eeq_sym.
+Definition eeq_trans : forall x y z, eeq x y -> eeq y z -> eeq x z := @Eeq.eeq_trans.
+
+(* Set membership *)
+Definition iin : set -> set -> Prop := Iin.iin.
+
+(* Extensionality *)
+Definition ext : forall x y, eeq x y <-> forall z, iin z x <-> iin z y := @Ext2.ext.
+
+(* TODO: put here set operators *)
+
+End NFO.
