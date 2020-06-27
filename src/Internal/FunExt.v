94d5707e7410133fbb76d79e8e6416e6a8886256
Andrea Condoluci <andrea.condoluci@tweag.io>
2020-06-27 13:27:33 +0200

    refactor

--- src/Internal/FunExt.v
+++ src/Internal/FunExt.v
@@ -37,11 +37,21 @@ Qed.
 
 (*  *)
 
-Lemma invert_sum_respects {X Y Z h h'} {R: Z -> Z -> Prop} {P: X + Y -> Prop}:
+Lemma invert_sum_respects {X Y Z f g} {R: Z -> Z -> Prop} {P: X + Y -> Prop}:
   Equivalence R ->
-    respects (R ⨀ (h ⨁ h')) P -> respects R (invert_sum P (R ∘ h) (R ∘ h')).
+    respects (R ⨀ (f ⨁ g)) P -> respects R (invert_sum P (R ∘ f) (R ∘ g)).
 Proof.
-  unfold respects in *. intros. intros.
-  destruct H.
+  unfold respects in *. intros. intros. destruct H.
   split; intros; repeat destruct H; firstorder.
+Qed.
+
+(*  *)
+
+Lemma ex_T_resp: forall {X Y R} P y (f: X -> Y),
+  respects R P ->
+    (exists ex: {x: X & P (f x) : Prop}, R (f (projT1 ex)) y) <-> P y /\ exists x, R (f x) y.
+Proof.
+  intros. setoid_rewrite (ex_T (fun X => P (f X)) (fun X => R (f X) y)).
+  split; intro H0; destruct H0. destruct H0; split; eauto. apply (H _ _ H1). auto.
+  firstorder.
 Qed.
--- src/NFO/Union.v
+++ src/NFO/Union.v
@@ -1,3 +1,5 @@
+(** NFO.Union : Union of NFO sets *)
+
 (* begin hide *)
 From Coq.Program Require Import Basics Combinators.
 Add LoadPath "src".
@@ -5,8 +7,7 @@ From Internal Require Import Aux FunExt.
 From NFO Require Import Xor BoolExpr Model Eeq Iin Morphs Sets Ext.
 (* end hide *)
 
-(** Union of NFO sets *)
-
+(** TODO: clean up and explain *)
 Definition aux {X Y Z: Type} (A2: X -> set) (Q1: Y -> set) (p1: boolean) (Q2: Z -> set) (p2: boolean) a := 
 (Qin a Q2 p2 -> Qin a Q1 p1)
 /\ (Qin a Q1 p1 -> (Ain a A2 <-> Qin a Q2 p2))
@@ -25,15 +26,6 @@ Definition cup B C :=
       (g ⨁ g') (Or (map inl e) (map inr e'))
 end.
 
-Lemma aaa: forall {X Y R} P z (f: X -> Y),
-  respects R P ->
-(exists xp : {x: X & P (f x) : Prop}, R (f (projT1 xp)) z) <-> P z /\ exists x, R (f x) z.
-Proof.
-  intros. setoid_rewrite (ex_T (fun X => P (f X)) (fun X => R (f X) z)).
-  split; intro H0; destruct H0. destruct H0; split; eauto. apply (H _ _ H1). auto.
-  firstorder.
-Qed.
-
 Lemma cup_ok x y z: iin z (cup x y) <-> iin z x \/ iin z y.
 Proof.
   destruct x, y. unfold cup. rewrite iin_unfold.
@@ -42,8 +34,8 @@ Proof.
   setoid_rewrite iin_unfold.
   unfold Ain, select.
   unfold compose.
-  setoid_rewrite (aaa (aux f0 g e g0 e0) z f).
-  setoid_rewrite (aaa (aux f g0 e0 g e) z f0).
+  setoid_rewrite (ex_T_resp (aux f0 g e g0 e0) z f).
+  setoid_rewrite (ex_T_resp (aux f g0 e0 g e) z f0).
   - unfold aux.
       classic (exists x, f x == z);
         classic (exists x, f0 x == z);
